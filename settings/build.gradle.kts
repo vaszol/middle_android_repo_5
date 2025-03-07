1cc0e06220911cf7a384209fa0b3a446e793e86d
vasiliy <vaszol@mail.ru>
2025-03-07 09:22:05 +0300

    новый модуль settings

--- settings/build.gradle.kts
+++ settings/build.gradle.kts
@@ -1,21 +1,19 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
-    namespace = "com.yandex.practicum.middle_homework_5"
+    namespace = "com.yandex.practicum.middle_homework_5.settings"
     compileSdk = 35
+
     defaultConfig {
-        applicationId = "com.yandex.practicum.middle_homework_5"
         minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -40,31 +38,19 @@ android {
 }
 
 dependencies {
+
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.paging.compose)
-    implementation(libs.goodle.gson)
-    implementation(libs.androidx.room.runtime)
-    implementation(libs.androidx.room.ktx)
-    implementation(libs.androidx.room.paging)
-    kapt(libs.room.kapt)
-    implementation(libs.squareup.retrofit2)
-    implementation(libs.koin.compose)
-    implementation(libs.androidx.work.manager.ktx)
     implementation(libs.androidx.data.store)
-    implementation(libs.androidx.navigation.compose)
+    implementation(libs.koin.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(project(":settings"))
 }
